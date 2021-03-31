03a2bff31e5462ddc2f8016b343e54f43e395520
chacha912 <dkwkdnwk912@gmail.com>
2021-04-01 01:06:18 +0900

    feat: Init do_fizzbuzz
    user_num got from user.
    print 1 to user_num to test it works

--- git-github/210325_commit_practice/fb.py
+++ git-github/210325_commit_practice/fb.py
@@ -1,5 +1,11 @@
 def test():
     print('hello')
 
+def do_fizzbuzz(num):
+    for i in range(1, num+1):
+        print(i)
+
 if __name__=='__main__':
     test()
+    user_num = int(input('Type the number: '))
+    do_fizzbuzz(user_num)
