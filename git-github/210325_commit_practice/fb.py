1d6efa3ed4a86fbae2b4a3cfb64181f04effb427
chacha912 <dkwkdnwk912@gmail.com>
2021-04-01 01:07:53 +0900

    feat: Print fizz if i is times of 3

--- git-github/210325_commit_practice/fb.py
+++ git-github/210325_commit_practice/fb.py
@@ -1,11 +1,10 @@
-def test():
-    print('hello')
-
 def do_fizzbuzz(num):
-    for i in range(1, num+1):
+    for i in range(1,num+1):
         print(i)
+        if i%3==0:
+            print('fizz')
+        else:
+            print(i)
 
 if __name__=='__main__':
-    test()
     user_num = int(input('Type the number: '))
-    do_fizzbuzz(user_num)
